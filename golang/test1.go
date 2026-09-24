3108097b7bf064b2915b9dfd2d2b849acf73842a
kayando <grabovskija52@gmail.com>
2026-09-25 00:01:20 +0300

    golang

---
+++ golang/test1.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var name1 string = "test"; //name type value
+	//name2 string = "test";
+	name3 := "test2";
+	name4 := 'A';
+
+	fmt.Println(name1);
+	//fmt.Println(name2);
+	fmt.Println(name3);
+	fmt.Println(name4)
+
+
+}
